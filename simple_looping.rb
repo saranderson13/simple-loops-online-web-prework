e5aa65792d1a398ed5b9a02d3aacc5ee7f534d1f
Sarah Anderson <saranderson13@gmail.com>
2019-03-14 23:20:53 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -63,5 +63,9 @@ def for_iterator(number_of_times)
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
+  for statement in (1..number_of_times) do
+    puts phrase
+  end
+  
 end
 
