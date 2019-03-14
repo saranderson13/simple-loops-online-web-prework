a8c36786979816b8932491303f9a487db1946ca2
Sarah Anderson <saranderson13@gmail.com>
2019-03-14 23:18:17 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -36,7 +36,7 @@ def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   counter = 0
   
-  while counter <= number_of_times
+  while counter < number_of_times
     puts phrase
     counter += 1
   end
