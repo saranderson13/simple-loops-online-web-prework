27e423e3ad2fdb9ec724abe6a5b375a6080739cd
Sarah Anderson <saranderson13@gmail.com>
2019-03-14 23:14:43 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -14,7 +14,7 @@ def loop_iterator(number_of_times)
   loop do
     phrase = "Welcome to Flatiron School's Web Development Course!"
     counter += 1
-    break if counter == number of times
+    break if counter == number_of_times
   end
 end
 
