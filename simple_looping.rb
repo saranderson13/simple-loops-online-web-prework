aef7a496df764abfc8f10b91134e74dc7637d731
Sarah Anderson <saranderson13@gmail.com>
2019-03-14 23:13:16 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -8,8 +8,12 @@ def loop_iterator(number_of_times)
   #   correct number of times?
   # Maybe we should keep count of the number of times we've puts out the 
   #  phrase and break when the counter hits the appropriate number...
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+  counter = 0
   
+  loop do
+    phrase = "Welcome to Flatiron School's Web Development Course!"
+    counter += 1
+    break if counter == number of times
 end
 
 def times_iterator(number_of_times)
